8741e0d5ee28969df8657714fcf4007ab7905f6a
Yaroslav Barlet <yaroslavbarlet@gmail.com>
2018-09-03 12:34:31 +0300

    Part 24

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Unit;
 
+use App\Post;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
@@ -14,6 +15,9 @@ class ExampleTest extends TestCase
      */
     public function testBasicTest()
     {
-        $this->assertTrue(true);
+//        $this->assertTrue(true);
+
+        $post = Post::archives();
+
     }
 }
